13d8741d7faaa495d28ee03a6cf492f5d22ea57f
freer <freer@mail.ru>
2020-10-13 15:21:19 +0300

    create 4 buttons

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myfirstandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+        buttonOne.setOnClickListener {
+            LogText.setText("Button One have been pressed");
+        };
+        buttonTwo.setOnClickListener {
+            LogText.setText("Button Two have been pressed");
+        };
+        buttonThree.setOnClickListener {
+            LogText.setText("Button Three have been pressed");
+        };
+        buttonFour.setOnClickListener {
+            LogText.setText("Button Four have been pressed");
+        };
     }
 }
